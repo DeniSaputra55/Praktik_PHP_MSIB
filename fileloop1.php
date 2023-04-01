c345c64e33182528abbf413d1823e644ad18cfa9
DeniSaputra <denisaputrafebruari55@gmail.com>
2023-04-01 14:11:43 +0700

    uplod file looping

---
+++ fileloop1.php
@@ -0,0 +1,40 @@
+<!-- praktek hari senin 27/03/2023 -->
+<?php
+//jika tidak menggunakan looping
+echo 'Bilangan 1';
+echo '<br> Bilangan 2';
+echo '<hr>';
+
+//ini contoh looping increment
+echo '<br> ini contoh looping increment';
+for($x=1; $x <= 10; $x++){
+    echo '<br> Bilangan '.$x;
+}
+//ini contoh looping decrement
+for($y=10; $y >= 1; $y--){
+    echo '<br> Angka '.$y;
+}
+echo '<hr>';
+//looping menggunakan while
+echo '<br> looping menggunakan while';
+$xz=1;
+while($xz <= 5){
+    echo '<br> Bilangan '.$xz;
+    $xz++;
+}
+
+$yz=5;
+while($yz >= 1){
+    echo '<br> angka '.$yz;
+    $yz--;
+}
+echo '<hr>';
+//contoh do while
+echo '<br> looping menggunakan  do while';
+$d=1;
+do {
+    echo '<br> '.$d;
+    $d++;
+}
+while($d<=10);
+?>
